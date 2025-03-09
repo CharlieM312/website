99ca54e5d8bdb4902e331b8c73d7111be8cb1965
CharlieM312 <cm2339@york.ac.uk>
2025-03-09 16:57:25 +0000

    General upgrades including to UI

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing-module';
 import { IndexComponent } from './index/index.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-    declarations: [AppComponent, IndexComponent, HeaderComponent],
+    declarations: [AppComponent, IndexComponent, HeaderComponent, AboutComponent],
     imports: [BrowserModule, AppRoutingModule],
     bootstrap: [AppComponent]
 })
