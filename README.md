1d73f53fa9fa4b7b661c5f9bf15fbbf1e99db00b
CharlieM312 <charliecmeader@gmail.com>
2025-08-31 17:14:15 +0100

    test updates

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ This project hosts my personal website... I will be getting a domain name for it
 
 [![Build Application](https://github.com/CharlieM312/website/actions/workflows/angular-build.yml/badge.svg?branch=main)](https://github.com/CharlieM312/website/actions/workflows/angular-build.yml)
 
+[![Test](https://github.com/CharlieM312/website/actions/workflows/test.yml/badge.svg?branch=main)](https://github.com/CharlieM312/website/actions/workflows/test.yml)
+
---
+++ app/src/app/index/index.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IndexComponent } from './index.component';
+import { AppModule } from '../app.module';
+
+describe('IndexComponent', () => {
+  let component: IndexComponent;
+  let fixture: ComponentFixture<IndexComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [IndexComponent],
+      imports: [AppModule]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(IndexComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
