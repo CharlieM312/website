66acebd3ff4aa7291d319624552404ea573b7140
CharlieM312 <73425406+CharlieM312@users.noreply.github.com>
2025-10-18 16:20:39 +0100

    Dark Mode updates (#49)

--- app/src/app/services/dark-mode/dark-mode.service.spec.ts
+++ app/src/app/services/dark-mode/dark-mode.service.spec.ts
@@ -1,5 +1,5 @@
-import { TestBed } from "@angular/core/testing";
-import { DarkModeService } from "./dark-mode.service";
+import { TestBed } from '@angular/core/testing';
+import { DarkModeService } from './dark-mode.service';
 
 
 describe('DarkModeService', () => {
