5f95247dc1913e0446ed4a3b6e06787316c152ec
CharlieM312 <73425406+CharlieM312@users.noreply.github.com>
2025-10-20 12:21:14 +0100

    Dark mode fixes (#50)
    * Dark Mode updates (#49)
    
    * Updates to build etc

--- app/src/app/settings/settings.component.ts
+++ app/src/app/settings/settings.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DarkModeService } from '../services/dark-mode/dark-mode.service';
+import { CookieService } from 'ngx-cookie-service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'settings',
@@ -7,21 +9,47 @@ import { DarkModeService } from '../services/dark-mode/dark-mode.service';
     styleUrl: './settings.component.scss',
     standalone: false
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit {
 
-    constructor(private darkModeService: DarkModeService) { }
+    constructor(private darkModeService: DarkModeService, private cookieService: CookieService) { }
 
     darkModeEnabled = false;
+    private subscriber?: Subscription;
 
-    toggleDarkMode() {
-        this.darkModeService.toggleDarkMode();
-        const content = document.querySelector('.content');
-        if (this.darkModeService.isDarkModeEnabled()) {
+    ngOnInit() {
+        this.darkModeEnabled = this.darkModeService.isDarkModeEnabled();
+        
+        if (this.cookieService.get('darkMode') === 'true') {
+            this.darkModeService.enableDarkMode();
+            const content = document.querySelector('.content');
             document.body.classList.add('dark-mode');
             if (content) content.classList.add('dark-mode');
-        } else {
+        } else if (this.cookieService.get('darkMode') === 'false') {
+            this.darkModeService.disableDarkMode();
+            const content = document.querySelector('.content');
             document.body.classList.remove('dark-mode');
             if (content) content.classList.remove('dark-mode');
         }
+
+        this.subscriber = this.darkModeService.darkMode$.subscribe(enabled => {
+            this.darkModeEnabled = enabled;
+            const content = document.querySelector('.content');
+
+            if (enabled) {
+                document.body.classList.add('dark-mode');
+                if (content) content.classList.add('dark-mode');
+                this.cookieService.set('darkMode', 'true');
+            } else {
+                document.body.classList.remove('dark-mode');
+                if (content) content.classList.remove('dark-mode');
+                this.cookieService.set('darkMode', 'false');
+            }
+
+        });
+
+    }
+
+    toggleDarkMode() {
+        this.darkModeService.toggleDarkMode();
     }
 }
