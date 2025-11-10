adecd908c17797748ead392fca9c3f640e000873
CharlieM312 <charliecmeader@gmail.com>
2025-11-10 21:09:27 +0000

    Map coordinates added

--- app/src/app/index/index.component.ts
+++ app/src/app/index/index.component.ts
@@ -18,6 +18,7 @@ export class IndexComponent implements OnInit, AfterViewInit, OnDestroy {
     private map?: L.Map;
     private lightLayer?: L.TileLayer;
     private darkLayer?: L.TileLayer;
+    englishAirportsVisible: boolean = true;
 
     ngOnInit() {
 
@@ -49,9 +50,9 @@ export class IndexComponent implements OnInit, AfterViewInit, OnDestroy {
 
     private initMap(): void {
         (L.Icon.Default as any).mergeOptions({
-            iconRetinaUrl: '/assets/leaflet-images/marker-icon-2x.png',
-            iconUrl: '/assets/leaflet-images/marker-icon.png',
-            shadowUrl: '/assets/leaflet-images/marker-shadow.png'
+            iconRetinaUrl: 'assets/leaflet-images/marker-icon-2x.png',
+            iconUrl: 'assets/leaflet-images/marker-icon.png',
+            shadowUrl: 'assets/leaflet-images/marker-shadow.png'
         });
 
         this.map = L.map('map', { center: [54.0, -2.0], zoom: 5 });
@@ -71,6 +72,35 @@ export class IndexComponent implements OnInit, AfterViewInit, OnDestroy {
         this.map.whenReady(() => this.map!.invalidateSize());
 
         setTimeout(() => this.map?.invalidateSize(), 200);
+
+        this.addAirports();
+    }
+
+    toggleEnglishAirports(){
+        // Logic to toggle English airports on the map
+    }
+
+    private addAirports(){
+        // London Airports
+        L.marker([51.4706, -0.4619]).bindPopup('Heathrow').addTo(this.map!);
+        this.map!.setView([51.4706, -0.4619], 7);
+        L.marker([51.1481, -0.1903]).bindPopup('Gatwick').addTo(this.map!);
+        L.marker([51.5053, 0.0553]).bindPopup('London City').addTo(this.map!);
+        // Other airports
+        L.marker([52.4539, 1.7486]).bindPopup('Norwich').addTo(this.map!);
+        L.marker([53.3807, -1.4701]).bindPopup('Leeds Bradford').addTo(this.map!);
+        L.marker([53.3537, -2.2746]).bindPopup('Manchester').addTo(this.map!);
+        // Ireland Airports
+        L.marker([53.3537, -6.2489]).bindPopup('Dublin').addTo(this.map!);
+        L.marker([53.4213, -6.2701]).bindPopup('Shannon').addTo(this.map!);
+        L.marker([55.0375, -1.6917]).bindPopup('Newcastle').addTo(this.map!);
+        // Scotland Airports
+        L.marker([57.1217, -2.2426]).bindPopup('Aberdeen').addTo(this.map!);
+        L.marker([55.8719, -4.4331]).bindPopup('Glasgow').addTo(this.map!);
+        L.marker([55.9500, -3.3725]).bindPopup('Edinburgh').addTo(this.map!);
+        // Welsh Airports
+        L.marker([51.3969, -3.3434]).bindPopup('Cardiff').addTo(this.map!);
+
     }
 
     private setTileLayer(dark: boolean) {
