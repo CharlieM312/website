0e01a49dcae0f1019661edcb17aeab83ebb95688
CharlieM312 <charliecmeader@gmail.com>
2025-12-27 16:51:14 +0000

    another update

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,10 +1,12 @@
-
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testMatch: ['**/*.spec.ts'],
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(@angular|@ngrx|tslib|ngx-cookie-service)/)'
+  ],
   reporters: [
     'default',
     ['jest-junit', { outputDirectory: './test-results', outputName: 'results.xml' }]
